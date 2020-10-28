921511473d10f00ca2fb631951a03d9c74c62029
Santos Ivan Alva Chavez <ivanals8998@gmail.com>
2020-10-28 18:13:01 +0100

    js-html-css-revolut -  add js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
 
+  // Refs
+  var dropLink = $('.parent > a');
+  var dropMenu = $('.parent > .menu');
+
+  // Show/hide menu
+  dropLink.click(function() {
+
+    var actualMenu = $(this).next('.menu');
+
+    dropMenu.not(actualMenu).hide();
+
+    actualMenu.toggle();
+
+  });
+
+});
